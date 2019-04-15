17e0933ec7cfb666399756960402a8ae5059d89f
Leandro Etcharren <etcharren.leo@gmail.com>
2019-04-15 02:10:59 -0300

    solucionado pequeño error controller

--- src/main/java/Controller/UsuarioController.java
+++ src/main/java/Controller/UsuarioController.java
@@ -1,5 +1,6 @@
 package Controller;
 
+import Model.Incidente;
 import Model.Usuario;
 import Utils.StandardResponse;
 import spark.Request;
@@ -9,23 +10,53 @@ import java.util.Collection;
 
 public class UsuarioController {
 
+    /**
+     * one implementation ArtistDao to connect to db
+     */
+    private static UsuarioController instance;
+    private static ProyectoController pcInstance;
+    private static IncidenteController icInstance;
+
+    /**
+     * Constructor
+     */
+    private  UsuarioController(){
+        ProyectoController.getInstance();
+        IncidenteController.getInstance();
+    }
+
+    /**
+     *  Method return SingletonInstance of ArtistController
+     */
+    public static UsuarioController getInstance() {
+        if (instance==null){
+            instance=new UsuarioController();
+        }
+        return instance;
+    }
+
     public static Collection<Usuario> getUsuarios(Request req, Response res) {
+
         return null;
     }
 
     public static Usuario getUsuario(Request req, Response res) {
+
         return null;
     }
 
     public static Usuario createUsuario(Request req, Response res) {
+
         return null;
     }
 
     public static Usuario setUsuario(Request req, Response res) {
+
         return null;
     }
 
     public static boolean deleteUsuario(Request req, Response res) {
+
         return false;
     }
 
