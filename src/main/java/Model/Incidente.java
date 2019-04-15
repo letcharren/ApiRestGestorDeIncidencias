ef50496bf4cdf89ad41c99e06923243fdd680621
Leandro Etcharren <etcharren.leo@gmail.com>
2019-04-15 02:53:48 -0300

    modificado modelo, agregado gett and set Incidente

--- src/main/java/Model/Incidente.java
+++ src/main/java/Model/Incidente.java
@@ -1,5 +1,7 @@
 package Model;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Incidente {
@@ -8,7 +10,90 @@ public class Incidente {
     String descripcion;
     Usuario reportador;
     Usuario responsable;
+    Proyecto proyecto;
     Estado estado;
     Date fechaCreacion;
-    Date fechaSoulcion;
+    Date fechaSolucion;
+
+    public Incidente(Clasificacion clasificacion, String descripcion, Usuario reportador, Usuario responsable, Proyecto proyecto, Estado estado) {
+        this.clasificacion = clasificacion;
+        this.descripcion = descripcion;
+        this.reportador = reportador;
+        this.responsable = responsable;
+        this.proyecto = proyecto;
+        this.estado = estado;
+        this.fechaCreacion = new Date();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Clasificacion getClasificacion() {
+        return clasificacion;
+    }
+
+    public void setClasificacion(Clasificacion clasificacion) {
+        this.clasificacion = clasificacion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Usuario getReportador() {
+        return reportador;
+    }
+
+    public void setReportador(Usuario reportador) {
+        this.reportador = reportador;
+    }
+
+    public Usuario getResponsable() {
+        return responsable;
+    }
+
+    public void setResponsable(Usuario responsable) {
+        this.responsable = responsable;
+    }
+
+    public Proyecto getProyecto() {
+        return proyecto;
+    }
+
+    public void setProyecto(Proyecto proyecto) {
+        this.proyecto = proyecto;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+
+    public Date getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(Date fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public Date getFechaSolucion() {
+        return fechaSolucion;
+    }
+
+    public void setFechaSolucion(Date fechaSolucion) {
+        this.fechaSolucion = fechaSolucion;
+    }
 }
--- src/main/java/Model/Proyecto.java
+++ src/main/java/Model/Proyecto.java
@@ -17,31 +17,38 @@ public class Proyecto {
     }
 
     public int getId() {
+
         return id;
     }
 
     public void setId(int id) {
+
         this.id = id;
     }
 
     public String getTitulo() {
+
         return titulo;
     }
 
     public void setTitulo(String titulo) {
+
         this.titulo = titulo;
     }
 
     public Usuario getPropietario() {
+
         return propietario;
     }
 
     public void setPropietario(Usuario propietario) {
+
         this.propietario = propietario;
     }
 
     @Override
     public boolean equals(Object o) {
+
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Proyecto proyecto = (Proyecto) o;
@@ -52,6 +59,7 @@ public class Proyecto {
 
     @Override
     public int hashCode() {
+
         return Objects.hash(id, titulo, propietario);
     }
 }
