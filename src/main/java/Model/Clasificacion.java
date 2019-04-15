d8386692b90391f2b05360312321b15757ccb378
Leandro Etcharren <etcharren.leo@gmail.com>
2019-04-15 13:17:09 -0300

    Implementado enumerados

--- src/main/java/Model/Clasificacion.java
+++ src/main/java/Model/Clasificacion.java
@@ -1,4 +1,24 @@
 package Model;
 
 public enum Clasificacion {
+    CRITICO("Critico"),
+    NORMAL("Normal"),
+    MENOR("Menor");
+
+
+    private String status;
+
+    Clasificacion(String status) {
+
+    }
+
+    public String getSClasificacion() {
+
+        return status;
+    }
+
+    public void setClasificacion(String status) {
+
+        this.status = status;
+    }
 }
--- src/main/java/Model/Estado.java
+++ src/main/java/Model/Estado.java
@@ -1,5 +1,22 @@
 package Model;
 
 public enum Estado {
+    RESUELTO("Resuelto"),
+    ASIGNADO("Asignado");
 
+    private String status;
+
+    Estado(String status) {
+
+    }
+
+    public String getStatus() {
+
+        return status;
+    }
+
+    public void setStauts(String status) {
+
+        this.status = status;
+    }
 }
